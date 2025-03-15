deb8fd8e4d992736b2cd957da5040314d020c5db
Benton Marcum <bjmarcum@gmail.com>
2025-03-15 12:04:47 -0600

    changes to code

--- code.js
+++ code.js
@@ -1,60 +1,11 @@
 function updateText(){
 
     // here would we put the condition for if we win or lose? Then have it change to different words
-if{
+
 
 
 document.getElementById("heading").innerHTML 
 = "You Won!?";
 
 
-}
 
-else{
-
-    
-    document.getElementById("heading").innerHTML 
-    = "You Lost!";
-
-}
-}
-
-
- import java.util.Random;
-
-// helps get a specific package to generate a random number
-
-public class Main {
-
-    public static void main(String[] args) {
-
-        Random random-new Random();
-// sets up an instance or one time call of a random number
-
-System.out.printIn("Random Integer:"+random.nextInt(7));
-// generates a number within 1-7
-
-System.out.printIn("Random Integer:"+random.nextInt(7));
-// two dice to be rolled, later determiend if player or CPU loses.
-
-
-    }
-}
-
-
-
-
-
-Function randomizeNumbers(0,7)
-let results = 0 + 7;
-
-return results;
-if (0<7) {
-
-    System.out.PrintIn("You Lose!");
-
-else{
-
-    System.out.PrintIn("You Win!")
-}
-}
