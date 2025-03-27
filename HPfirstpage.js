f3552611645cc3d2a6a1741a90f6da52a32e1e71
Benton Marcum <bjmarcum@gmail.com>
2025-03-26 20:11:37 -0600

    code update 3/25

--- HPfirstpage.js
+++ HPfirstpage.js
@@ -1,32 +1,33 @@
 //Benton Marcum Code
-function choose()
+
+
+
+function snitchPlay()
 {
-
-
-    document.getElementById("loginstatus").innerHTML = "Welcome!" + " " + fullName; 
-    location.replace ("ChoosebroomHP.html");
-
-
-    mySound = new sound("put the sound here");
-    mySound.play();
-    document.getElementById("playerbutton").disabled = true;
-// sound for selecting, "Nice choice!"
 
 
 
 }
 
-
-function broomSelect()
+function play()
 {
+    mySound = new sound("soundcs102.wav");
+    mySound.play();
+    document.getElementById("snitchbutton").disabled = true;
 
+    // this turns off the start button, disabling it equaling true
+    document.getElementById("stopbutton").disabled = false;
+    // this turns on the stop button since it starts disabled and becomes false
+}
 
-<input type = "submit" onclick = "broomSelected();"></input>
-location.replace ("snitchpage.html");
+function sound (src)
+{
+// add play function
+    this.sound = document.createElement("audio") ;//create audio
+    this.sound.src = src;//define src
+    this.play = function() {//music function
+        this.sound.play();//play sound
+    }
 
-mySound = new sound("put the sound here");
-mySound.play();
-document.getElementById("choosebroom").disabled = true;
-
-
+    
 }
